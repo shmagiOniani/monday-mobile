78bd4e6b03fe6bcb543112f48b65695205e17350
shmagi.onaini <onianishmagi@gmail.com>
2023-12-04 10:00:02 +0400

    init project

--- App.js
+++ App.js
@@ -1,9 +1,5 @@
 import { Text, SafeAreaView, StyleSheet } from 'react-native';
-
-// You can import supported modules from npm
 import { Card } from 'react-native-paper';
-
-// or any files within the Snack
 import AssetExample from './components/AssetExample';
 
 export default function App() {
